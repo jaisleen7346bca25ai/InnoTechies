27782605cd75ee86601ccfee1ada913e33545b45
jaisleen7346bca25ai <jaisleen7346.bca25ai@chitkara.edu.in>
2025-09-24 11:37:29 +0530

    Add files via upload
    InnoTechies riddle 3

---
+++ riddle3.py
@@ -0,0 +1,9 @@
+temp= int(input("Enter the Temperature: "))
+if (temp<0):
+    print("The colour of the potion is BLUE")
+elif (temp>100):
+    print("The colour of the potion is RED")
+elif (temp==50):
+    print("The colour of the potion is PURPLE")
+else:
+    print("The colour of the potion is CLEAR")
